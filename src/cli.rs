4a98fe2fc1af38bfdbe4c8fa58579f35ba3ac43f
Daniel Ramirez-Chiquillo <hi@danielrachi.com>
2025-07-27 13:59:11 -0500

    refactor: move cli and lib to separate files

---
+++ src/cli.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// Path to the root of the life directory
+    #[clap(long, value_name = "PATH")]
+    pub life_path: String,
+}
+
+pub fn parse_args() -> Args {
+    Args::parse()
+} 
